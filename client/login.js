92c5ee28466141498a04cb2a9203c15d884f0f79
kjm90110 <kjm90118730@gmail.com>
2025-11-25 19:17:29 +0900

    로그인 화면, 로그인 입력값 검증, 회원가입 화면 구성 완료

--- client/login.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("submit", () => {
-    const id = document.getElementById("userId");
-    const pw = document.getElementById("password");
-
-    const auth = /^[a-zA-Z0-9!@#$%^&*()]{8,}/; // 영대소문자, 숫자, 특수문자 포함 8자 이상
-});
---
+++ client/user/user.js
@@ -0,0 +1,31 @@
+const loginBtn = document.getElementById("loginBtn");
+const signUpBtn = document.getElementById("signUpBtn");
+
+// 로그인 버튼 클릭 이벤트
+loginBtn.addEventListener("click", () => {
+    const id = document.getElementById("userId").value;
+    const pw = document.getElementById("password").value;
+
+    console.log(id);
+    console.log(pw);
+
+    // 영대소문자, 숫자, 특수문자 포함 8자 이상
+    // 아이디에 한글이 포함되지 않아야 함
+    const pwAuth = /^[\w!@#$%^&*()]{8,}[^\s]$/;
+    const hasKorean = /[ㄱ-ㅎ가-힣]/;
+
+    const idTest = hasKorean.test(id); // id 형식 검사
+
+    // password는 input type이 password이기 때문에 한글 입력이 안됨
+    const pwTest = pwAuth.test(pw); // pw 형식 검사
+
+    if (!idTest && !pwTest) {
+        alert("아이디 또는 비밀번호를 확인해 주세요.");
+        return;
+    }
+
+    alert("로그인 성공!");
+    return;
+});
+
+signUpBtn.addEventListener("click", () => {});
